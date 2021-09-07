06ba327bf6d390ac921e08bdc4ce45ff31c10f13
mbantwerpes <marcel.blesing@antwerpes.com>
2021-09-07 11:09:55 +0200

    Use techcomponent routes in server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,14 @@ dotenv.config();
 
 import express from 'express';
 
+import techcomponent from './lib/routes/techComponent';
+
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
+
+app.use('/api/techcomponent', techcomponent);
 
 app.get('/api', (_request, response) => {
   response.json({ message: 'Hello API!' });
