8c4d2f921398a5e2dc4c0e3555337672e849892c
mbantwerpes <marcel.blesing@antwerpes.com>
2021-09-07 14:07:52 +0200

    Add utils function to get current time as string

---
+++ src/lib/utils/time.ts
@@ -0,0 +1,10 @@
+export const getCurrentDate = (): string => {
+  const today = new Date();
+  const dd = String(today.getDate()).padStart(2, '0');
+  const mm = String(today.getMonth() + 1).padStart(2, '0');
+  const yyyy = today.getFullYear();
+
+  const stringDate = `${yyyy}-${mm}-${dd}`;
+
+  return stringDate;
+};
