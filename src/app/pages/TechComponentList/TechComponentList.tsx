27ac5eaedbbc94bccea3e4d9e9480233f4664ad4
mbantwerpes <marcel.blesing@antwerpes.com>
2021-09-09 14:37:39 +0200

    Add header to page

--- src/app/pages/TechComponentList/TechComponentList.tsx
+++ src/app/pages/TechComponentList/TechComponentList.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import FloatingActionButton from '../../components/FloatinActionButton/FloatingActionButton';
+import Typography from '../../components/Typography/Typography';
 import styles from './TechComponentList.module.css';
+import { MdShoppingCart } from 'react-icons/md';
 
 const TechComponentList = (): JSX.Element => {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <Typography type="header" size="xl">
+          Bauteile.
+        </Typography>
+        <FloatingActionButton icon={<MdShoppingCart size={24} />} />
+      </div>
+    </div>
+  );
 };
 
 export default TechComponentList;
