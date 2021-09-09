d7687b297bb0ed91fe567cf8dbccd6395b2ff2fd
mbantwerpes <marcel.blesing@antwerpes.com>
2021-09-09 14:30:50 +0200

    Add setup for TechComponentList page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import TechComponentList from './pages/TechComponentList/TechComponentList';
 
 const App = (): JSX.Element => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/">
-          <main>Home</main>
+          <TechComponentList />
         </Route>
       </Switch>
       <div id="modal-root" />
---
+++ src/app/pages/TechComponentList/TechComponentList.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './TechComponentList.module.css';
+
+const TechComponentList = (): JSX.Element => {
+  return <div className={styles.container}></div>;
+};
+
+export default TechComponentList;
