cd4b423b4b7133ed52630e127914a2422e6c04ee
mbantwerpes <marcel.blesing@antwerpes.com>
2021-09-09 15:36:51 +0200

    Refactor layout

--- src/app/pages/TechComponentList/TechComponentList.tsx
+++ src/app/pages/TechComponentList/TechComponentList.tsx
@@ -10,57 +10,73 @@ import Navbar from '../../components/Navbar/Navbar';
 
 const TechComponentList = (): JSX.Element => {
   return (
-    <div className={styles.container}>
-      <div className={styles.header}>
-        <Typography type="header" size="xl">
-          Bauteile.
-        </Typography>
-        <FloatingActionButton icon={<MdShoppingCart size={24} />} />
-      </div>
-      <Input
-        type="text"
-        value=""
-        onChange={() => console.log('test')}
-        placeholder="Bauteil suchen..."
-        icon={<MdSearch />}
-        containerStyling={styles.inputContainer}
-      />
-      <div className={styles.cardList}>
-        <TechComponentCard
-          layout="vertical"
-          title="Microcontroller A-30"
-          description="Lorem ipsum dolor sit amet."
-          amount={10}
-          image={placeholderImage}
+    <div className={styles.layout}>
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <Typography type="header" size="xl">
+            Bauteile.
+          </Typography>
+          <FloatingActionButton icon={<MdShoppingCart size={24} />} />
+        </div>
+        <Input
+          type="text"
+          value=""
+          onChange={() => console.log('test')}
+          placeholder="Bauteil suchen..."
+          icon={<MdSearch />}
+          containerStyling={styles.inputContainer}
         />
-        <TechComponentCard
-          layout="vertical"
-          title="Microcontroller A-30"
-          description="Lorem ipsum dolor sit amet."
-          amount={10}
-          image={placeholderImage}
-        />
-        <TechComponentCard
-          layout="vertical"
-          title="Microcontroller A-30"
-          description="Lorem ipsum dolor sit amet."
-          amount={10}
-          image={placeholderImage}
-        />
-        <TechComponentCard
-          layout="vertical"
-          title="Microcontroller A-30"
-          description="Lorem ipsum dolor sit amet."
-          amount={10}
-          image={placeholderImage}
-        />
-        <TechComponentCard
-          layout="vertical"
-          title="Microcontroller A-30"
-          description="Lorem ipsum dolor sit amet."
-          amount={10}
-          image={placeholderImage}
-        />
+        <div className={styles.cardList}>
+          <TechComponentCard
+            layout="vertical"
+            title="Microcontroller A-30"
+            description="Lorem ipsum dolor sit amet."
+            amount={10}
+            image={placeholderImage}
+          />
+          <TechComponentCard
+            layout="vertical"
+            title="Microcontroller A-30"
+            description="Lorem ipsum dolor sit amet."
+            amount={10}
+            image={placeholderImage}
+          />
+          <TechComponentCard
+            layout="vertical"
+            title="Microcontroller A-30"
+            description="Lorem ipsum dolor sit amet."
+            amount={10}
+            image={placeholderImage}
+          />
+          <TechComponentCard
+            layout="vertical"
+            title="Microcontroller A-30"
+            description="Lorem ipsum dolor sit amet."
+            amount={10}
+            image={placeholderImage}
+          />
+          <TechComponentCard
+            layout="vertical"
+            title="Microcontroller A-30"
+            description="Lorem ipsum dolor sit amet."
+            amount={10}
+            image={placeholderImage}
+          />
+          <TechComponentCard
+            layout="vertical"
+            title="Microcontroller A-30"
+            description="Lorem ipsum dolor sit amet."
+            amount={10}
+            image={placeholderImage}
+          />
+          <TechComponentCard
+            layout="vertical"
+            title="Microcontroller A-30"
+            description="Lorem ipsum dolor sit amet."
+            amount={10}
+            image={placeholderImage}
+          />
+        </div>
       </div>
       <Navbar active="home" />
     </div>
