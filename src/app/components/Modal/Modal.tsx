e1f60be824cdfcc613301bd7b7a10bec8a8ddde1
mbantwerpes <marcel.blesing@antwerpes.com>
2021-09-09 10:05:33 +0200

    Add modal with styling

---
+++ src/app/components/Modal/Modal.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import styles from './Modal.module.css';
+
+type ModalProps = {
+  children: React.ReactChild;
+  closeModal: () => void;
+  title: string;
+};
+
+const Modal = React.memo(({ children, closeModal, title }: ModalProps) => {
+  const domEl = document.querySelector('#modal-root');
+
+  if (!domEl) return null;
+
+  // This is where the magic happens -> our modal div will be rendered into our 'modal-root' div, no matter where we
+  // use this component inside our React tree
+  return ReactDOM.createPortal(
+    <div className={styles.container}>
+      <div className={styles.titleContainer}>
+        <h3>{title}</h3>
+        <button onClick={closeModal}>X</button>
+      </div>
+      {children}
+    </div>,
+    domEl
+  );
+});
+
+export default Modal;
