f08b538f82b0b5d477ef29975a1ca718a9cdfee5
mbantwerpes <marcel.blesing@antwerpes.com>
2021-09-09 15:03:45 +0200

    Create grid layout for page

--- src/app/pages/TechComponentList/TechComponentList.tsx
+++ src/app/pages/TechComponentList/TechComponentList.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import FloatingActionButton from '../../components/FloatinActionButton/FloatingActionButton';
 import Typography from '../../components/Typography/Typography';
 import styles from './TechComponentList.module.css';
-import { MdShoppingCart } from 'react-icons/md';
+import { MdShoppingCart, MdSearch } from 'react-icons/md';
+import Input from '../../components/Input/Input';
+import TechComponentCard from '../../components/TechComponentCard/TechComponentCard';
+import placeholderImage from '../../../assets/images/placeholder_image.jpeg';
+import Navbar from '../../components/Navbar/Navbar';
 
 const TechComponentList = (): JSX.Element => {
   return (
@@ -13,6 +17,52 @@ const TechComponentList = (): JSX.Element => {
         </Typography>
         <FloatingActionButton icon={<MdShoppingCart size={24} />} />
       </div>
+      <Input
+        type="text"
+        value=""
+        onChange={() => console.log('test')}
+        placeholder="Bauteil suchen..."
+        icon={<MdSearch />}
+        containerStyling={styles.inputContainer}
+      />
+      <div className={styles.cardList}>
+        <TechComponentCard
+          layout="vertical"
+          title="Microcontroller A-30"
+          description="Lorem ipsum dolor sit amet."
+          amount={10}
+          image={placeholderImage}
+        />
+        <TechComponentCard
+          layout="vertical"
+          title="Microcontroller A-30"
+          description="Lorem ipsum dolor sit amet."
+          amount={10}
+          image={placeholderImage}
+        />
+        <TechComponentCard
+          layout="vertical"
+          title="Microcontroller A-30"
+          description="Lorem ipsum dolor sit amet."
+          amount={10}
+          image={placeholderImage}
+        />
+        <TechComponentCard
+          layout="vertical"
+          title="Microcontroller A-30"
+          description="Lorem ipsum dolor sit amet."
+          amount={10}
+          image={placeholderImage}
+        />
+        <TechComponentCard
+          layout="vertical"
+          title="Microcontroller A-30"
+          description="Lorem ipsum dolor sit amet."
+          amount={10}
+          image={placeholderImage}
+        />
+      </div>
+      <Navbar active="home" />
     </div>
   );
 };
