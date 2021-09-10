677e49c06a97b48fe30016d710a1592a7b85779f
mbantwerpes <marcel.blesing@antwerpes.com>
2021-09-10 14:50:03 +0200

    Add layout styling

--- src/app/pages/TechComponentDetail/TechComponentDetail.tsx
+++ src/app/pages/TechComponentDetail/TechComponentDetail.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MdKeyboardArrowLeft, MdRemove } from 'react-icons/md';
+import { MdKeyboardArrowLeft, MdDelete } from 'react-icons/md';
 import { useHistory } from 'react-router-dom';
 import Button from '../../components/Button/Button';
 import Typography from '../../components/Typography/Typography';
@@ -19,49 +19,51 @@ const TechComponentDetail = (): JSX.Element => {
         <MdKeyboardArrowLeft size={32} />
       </Button>
       <Typography type="header" size="xl">
-        Bauteil anlegen.
+        Microcontroller A-30
       </Typography>
-      <img src={placeholderImage} alt="placeholder" />
-      <Typography type="header" size="m">
-        Anzahl: 10
-      </Typography>
-      <div>
+      <div className={styles.content}>
+        <img src={placeholderImage} alt="placeholder" className={styles.img} />
         <Typography type="header" size="m">
-          Artikelnummer
+          Verfügbar: 10
         </Typography>
-        <Typography type="text" size="m">
-          3284zhfdsakjfz348
-        </Typography>
+        <div>
+          <Typography type="header" size="m">
+            Artikelnummer
+          </Typography>
+          <Typography type="text" size="m">
+            3284zhfdsakjfz348
+          </Typography>
+        </div>
+        <div>
+          <Typography type="header" size="m">
+            Beschreibung
+          </Typography>
+          <Typography type="text" size="m">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis quia
+            incidunt dolorum animi? Atque officia, laudantium porro molestiae
+            eaque nulla, sequi exercitationem perferendis id voluptates dolor
+            corrupti labore consequatur quasi!
+          </Typography>
+        </div>
+        <div>
+          <Typography type="header" size="m">
+            Ortsangabe
+          </Typography>
+          <Typography type="text" size="m">
+            3. Schrank rechts
+          </Typography>
+        </div>
       </div>
-      <div>
-        <Typography type="header" size="m">
-          Beschreibung
-        </Typography>
-        <Typography type="text" size="m">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis quia
-          incidunt dolorum animi? Atque officia, laudantium porro molestiae
-          eaque nulla, sequi exercitationem perferendis id voluptates dolor
-          corrupti labore consequatur quasi!
-        </Typography>
-      </div>
-      <div>
-        <Typography type="header" size="m">
-          Ortsangabe
-        </Typography>
-        <Typography type="text" size="m">
-          3. Schrank rechts
-        </Typography>
-      </div>
-      <div>
+      <div className={styles.buttonGroup}>
         <Button
           type="error"
           size="l"
           onClick={() => console.log('placeholder')}
         >
-          <MdRemove />
+          <MdDelete size={24} />
         </Button>
         <Button
-          type="error"
+          type="primary"
           size="l"
           onClick={() => console.log('placeholder')}
         >
