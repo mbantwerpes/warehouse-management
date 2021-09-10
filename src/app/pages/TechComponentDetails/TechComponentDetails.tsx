6cc4a440801bc4008d892be8d8c18b9ac360bc85
mbantwerpes <marcel.blesing@antwerpes.com>
2021-09-10 14:20:45 +0200

    Basic setup for tech component details page

---
+++ src/app/pages/TechComponentDetails/TechComponentDetails.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './TechComponentDetails.module.css';
+
+const TechComponentDetails = (): JSX.Element => {
+  return <div className={styles.container}></div>;
+};
+
+export default TechComponentDetails;
