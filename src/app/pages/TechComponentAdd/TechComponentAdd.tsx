f892b59e5851fe6d6553a475f49a1d17151446ba
mbantwerpes <marcel.blesing@antwerpes.com>
2021-09-10 10:09:25 +0200

    Add inputs

--- src/app/pages/TechComponentAdd/TechComponentAdd.tsx
+++ src/app/pages/TechComponentAdd/TechComponentAdd.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router';
-// import Typography from '../../components/Typography/Typography';
+import Typography from '../../components/Typography/Typography';
 import styles from './TechComponentAdd.module.css';
-// import Input from '../../components/Input/Input';
+import Input from '../../components/Input/Input';
 import Navbar from '../../components/Navbar/Navbar';
 import Button from '../../components/Button/Button';
 import { MdKeyboardArrowLeft } from 'react-icons/md';
 
 const TechComponentAdd = (): JSX.Element => {
+  const [nameValue, setNameValue] = useState('');
+  const [artNrValue, setArtNrValue] = useState('');
+  const [locationValue, setLocationValue] = useState('');
+
   const history = useHistory();
 
   const handleBackButtonClick = () => {
@@ -16,10 +20,47 @@ const TechComponentAdd = (): JSX.Element => {
 
   return (
     <div className={styles.layout}>
-      <div className={styles.container}></div>
-      <Button type="secondary" size="l" onClick={handleBackButtonClick}>
-        <MdKeyboardArrowLeft />
-      </Button>
+      <div className={styles.container}>
+        <Button type="secondary" size="l" onClick={handleBackButtonClick}>
+          <MdKeyboardArrowLeft />
+        </Button>
+        <Typography type="header" size="xl">
+          Bauteil anlegen.
+        </Typography>
+        <label>
+          <Typography type="text" size="s">
+            Name
+          </Typography>
+          <Input
+            value={nameValue}
+            placeholder="Name"
+            type="text"
+            onChange={(e) => setNameValue(e.target.value)}
+          />
+        </label>
+        <label>
+          <Typography type="text" size="s">
+            Name
+          </Typography>
+          <Input
+            value={artNrValue}
+            placeholder="Artikelnummer"
+            type="text"
+            onChange={(e) => setArtNrValue(e.target.value)}
+          />
+        </label>
+        <label>
+          <Typography type="text" size="s">
+            Name
+          </Typography>
+          <Input
+            value={locationValue}
+            placeholder="Ortsangabe"
+            type="text"
+            onChange={(e) => setLocationValue(e.target.value)}
+          />
+        </label>
+      </div>
       <Navbar active="home" />
     </div>
   );
