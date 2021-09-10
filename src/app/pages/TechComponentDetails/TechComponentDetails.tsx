a2b8b76591866d54d799a1a15ae3eb782dcdcd30
mbantwerpes <marcel.blesing@antwerpes.com>
2021-09-10 14:22:03 +0200

    Add header

--- src/app/pages/TechComponentDetails/TechComponentDetails.tsx
+++ src/app/pages/TechComponentDetails/TechComponentDetails.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import { MdKeyboardArrowLeft } from 'react-icons/md';
+import { useHistory } from 'react-router-dom';
+import Button from '../../components/Button/Button';
+import Typography from '../../components/Typography/Typography';
 import styles from './TechComponentDetails.module.css';
 
 const TechComponentDetails = (): JSX.Element => {
-  return <div className={styles.container}></div>;
+  const history = useHistory();
+
+  const handleBackButtonClick = () => {
+    history.push('/');
+  };
+
+  return (
+    <div className={styles.container}>
+      <Button type="secondary" size="m" onClick={handleBackButtonClick}>
+        <MdKeyboardArrowLeft size={32} />
+      </Button>
+      <Typography type="header" size="xl">
+        Bauteil anlegen.
+      </Typography>
+    </div>
+  );
 };
 
 export default TechComponentDetails;
