69c1d1f305788ec6aabe377a69dcd727b3f8ff17
mbantwerpes <marcel.blesing@antwerpes.com>
2021-09-13 10:23:00 +0200

    Outsource form

---
+++ src/app/components/TechComponentForm/TechComponentForm.stories.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import TechComponentForm from './TechComponentForm';
+import { Story } from '@storybook/react';
+import type { TechComponentFormProps } from './TechComponentForm';
+
+export default {
+  title: 'Component/TechComponentForm',
+  component: TechComponentForm,
+  parameters: {
+    backgrounds: {
+      default: 'dark',
+    },
+  },
+  argTypes: {
+    layout: {
+      options: ['horizontal', 'vertical'],
+      control: { type: 'select' },
+    },
+  },
+};
+
+const Template: Story<TechComponentFormProps> = (args) => (
+  <TechComponentForm {...args} />
+);
+
+export const Add = Template.bind({});
+Add.args = {
+  titleValue: '',
+  artNrValue: '',
+  locationValue: '',
+  descriptionValue: '',
+  amountValue: '',
+};
