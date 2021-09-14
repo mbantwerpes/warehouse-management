c2d7f9c8ab4f296727fca94d46b0bd2d8748d964
mbantwerpes <marcel.blesing@antwerpes.com>
2021-09-14 15:26:14 +0200

    Change layout

--- src/app/pages/OrderDetail/OrderDetail.tsx
+++ src/app/pages/OrderDetail/OrderDetail.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { MdKeyboardArrowLeft } from 'react-icons/md';
 import { useHistory, useParams } from 'react-router-dom';
 import Button from '../../components/Button/Button';
+import TechComponentCard from '../../components/TechComponentCard/TechComponentCard';
 import Typography from '../../components/Typography/Typography';
 import useOrder from '../../hooks/useOrder';
 import useTechComponents from '../../hooks/useTechComponents';
 import styles from './OrderDetail.module.css';
+import placeholderImage from '../../../assets/images/placeholder_image.jpeg';
 
 const OrderDetail = (): JSX.Element => {
   const history = useHistory();
@@ -41,39 +43,62 @@ const OrderDetail = (): JSX.Element => {
           Ausleihauftrag.
         </Typography>
       </header>
-      <div className={styles.techComponentDetails}>
-        <Typography type="header" size="m">
-          Name:
-        </Typography>
-        <Typography type="text" size="l">
-          {order?._id}
-        </Typography>
-      </div>
-      <div className={styles.techComponentDetails}>
-        <Typography type="header" size="m">
-          Erstellt:
-        </Typography>
-        <Typography type="text" size="l">
-          {order?.crAt}
-        </Typography>
-      </div>
-      <div className={styles.techComponentDetails}>
-        <Typography type="header" size="m">
-          Rückgabefrist:
-        </Typography>
-        <Typography type="text" size="l">
-          {order?.returnPeriod ? order.returnPeriod : '-'}
-        </Typography>
-      </div>
-      <div className={styles.techComponentDetails}>
-        <Typography type="header" size="m">
-          Status:
-        </Typography>
-        <Typography type="text" size="l">
-          {order?.status}
-        </Typography>
-      </div>
-      <div className={styles.content}></div>
+      <section>
+        <div className={styles.techComponentDetails}>
+          <Typography type="header" size="m">
+            Name:
+          </Typography>
+          <Typography type="text" size="l">
+            {order?._id}
+          </Typography>
+        </div>
+        <div className={styles.techComponentDetails}>
+          <Typography type="header" size="m">
+            Erstellt:
+          </Typography>
+          <Typography type="text" size="l">
+            {order?.crAt}
+          </Typography>
+        </div>
+        <div className={styles.techComponentDetails}>
+          <Typography type="header" size="m">
+            Rückgabefrist:
+          </Typography>
+          <Typography type="text" size="l">
+            {order?.returnPeriod ? order.returnPeriod : '-'}
+          </Typography>
+        </div>
+        <div className={styles.techComponentDetails}>
+          <Typography type="header" size="m">
+            Status:
+          </Typography>
+          <Typography type="text" size="l">
+            {order?.status}
+          </Typography>
+        </div>
+      </section>
+      <section className={styles.content}>
+        {techComponents?.map((techComponent) => {
+          const orderTechComponent = order?.techComponents.find(
+            (orderTechComponent) =>
+              orderTechComponent.techComponentId === techComponent._id
+          );
+          if (orderTechComponent) {
+            techComponent.amount = orderTechComponent.amount;
+          }
+          return (
+            <TechComponentCard
+              key={techComponent._id}
+              id={techComponent._id}
+              layout="horizontal"
+              amount={techComponent.amount}
+              description={techComponent.description}
+              image={placeholderImage}
+              title={techComponent.title}
+            />
+          );
+        })}
+      </section>
       <Button
         type="primary"
         size="l"
