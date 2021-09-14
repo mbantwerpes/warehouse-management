264d325bd17dd7c6a91e1f8be258af46d4faf41b
mbantwerpes <marcel.blesing@antwerpes.com>
2021-09-14 12:08:50 +0200

    Add files for OrderList apge

---
+++ src/app/pages/OrderList/OrderList.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './OrderList.module.css';
+
+const OrderList = (): JSX.Element => {
+  return <div className={styles.container}></div>;
+};
+
+export default OrderList;
