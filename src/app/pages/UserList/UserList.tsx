257bbc3716f133ad502c3ddead20eba0edabe122
mbantwerpes <marcel.blesing@antwerpes.com>
2021-09-14 18:57:38 +0200

    Add basic setup for user page

---
+++ src/app/pages/UserList/UserList.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import FloatingActionButton from '../../components/FloatinActionButton/FloatingActionButton';
+import Typography from '../../components/Typography/Typography';
+import styles from './UserList.module.css';
+import { MdSearch, MdAdd } from 'react-icons/md';
+import Input from '../../components/Input/Input';
+import Navbar from '../../components/Navbar/Navbar';
+import { useHistory } from 'react-router-dom';
+
+const UserList = (): JSX.Element => {
+  const [searchValue, setSearchValue] = useState<string>('');
+
+  const history = useHistory();
+
+  const handleAddUser = () => {
+    history.push('/techcomponent/add');
+  };
+
+  const handleCardClick = (id: string) => {
+    history.push(`/techcomponent/${id}`);
+  };
+
+  //   const { users } = useUsers();
+
+  return (
+    <div className={styles.layout}>
+      <div className={styles.container}>
+        <header className={styles.header}>
+          <Typography type="header" size="xl">
+            Nutzer.
+          </Typography>
+          <FloatingActionButton
+            onClick={handleAddUser}
+            icon={<MdAdd size={24} />}
+          />
+        </header>
+        <Input
+          type="text"
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+          placeholder="Nutzer suchen..."
+          icon={<MdSearch />}
+          containerStyling={styles.inputContainer}
+        />
+        <section className={styles.cardList}></section>
+      </div>
+      <Navbar active="user" />
+    </div>
+  );
+};
+
+export default UserList;
