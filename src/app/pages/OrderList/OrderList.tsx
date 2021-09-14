82f0cafff59d9b0f82f601d014e21ba7f7ae3be7
mbantwerpes <marcel.blesing@antwerpes.com>
2021-09-14 12:29:08 +0200

    Add layout and styling

--- src/app/pages/OrderList/OrderList.tsx
+++ src/app/pages/OrderList/OrderList.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import Navbar from '../../components/Navbar/Navbar';
+import OrderCard from '../../components/OrderCard/OrderCard';
+import Typography from '../../components/Typography/Typography';
 import useOrders from '../../hooks/useOrders';
 import styles from './OrderList.module.css';
 
@@ -6,7 +9,62 @@ const OrderList = (): JSX.Element => {
   const { orders } = useOrders();
   console.log(orders);
 
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.layout}>
+      <div className={styles.container}>
+        <header className={styles.header}>
+          <Typography type="header" size="xl">
+            Ausleihaufträge.
+          </Typography>
+        </header>
+        <section className={styles.contentContainer}>
+          <OrderCard
+            name="123"
+            reservedAt="123"
+            returnAt="123"
+            status="reserviert"
+          />
+          <OrderCard
+            name="123"
+            reservedAt="123"
+            returnAt="123"
+            status="reserviert"
+          />
+          <OrderCard
+            name="123"
+            reservedAt="123"
+            returnAt="123"
+            status="reserviert"
+          />
+          <OrderCard
+            name="123"
+            reservedAt="123"
+            returnAt="123"
+            status="reserviert"
+          />
+          <OrderCard
+            name="123"
+            reservedAt="123"
+            returnAt="123"
+            status="reserviert"
+          />
+          <OrderCard
+            name="123"
+            reservedAt="123"
+            returnAt="123"
+            status="reserviert"
+          />
+          <OrderCard
+            name="123"
+            reservedAt="123"
+            returnAt="123"
+            status="reserviert"
+          />
+        </section>
+      </div>
+      <Navbar active="order" />
+    </div>
+  );
 };
 
 export default OrderList;
