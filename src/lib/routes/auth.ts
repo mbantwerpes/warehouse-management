e65eb53c853c3a2cf609d73a923410e205cad437
mbantwerpes <marcel.blesing@antwerpes.com>
2021-09-15 12:02:27 +0200

    Create jwt and set it as cookie

--- src/lib/routes/auth.ts
+++ src/lib/routes/auth.ts
@@ -1,13 +1,22 @@
 import { Router } from 'express';
 import { getUserByEmailAndPassword } from '../models/user';
+import jwt from 'jsonwebtoken';
 
 const router = Router();
 router.post('/', async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await getUserByEmailAndPassword(email, password);
-
-    res.json(user);
+    const secret = process.env.JWT_SECRET;
+    if (user && secret) {
+      const payload = { email };
+      const token = jwt.sign(payload, secret, {
+        expiresIn: '1h',
+      });
+      res.cookie('token', token, { httpOnly: true }).sendStatus(200);
+    } else {
+      res.json(user);
+    }
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
