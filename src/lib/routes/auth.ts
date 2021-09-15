486b4236ce95637020eaf037ad91be669f5c17f3
mbantwerpes <marcel.blesing@antwerpes.com>
2021-09-15 11:58:33 +0200

    Outsource auth into own route

--- src/lib/routes/auth.ts
+++ src/lib/routes/auth.ts
@@ -6,6 +6,7 @@ router.post('/', async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await getUserByEmailAndPassword(email, password);
+
     res.json(user);
   } catch (err) {
     res.status(500).json({ message: err.message });
