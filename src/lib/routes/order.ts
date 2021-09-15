e746fb36dca25f7ef83c4a1f4e2f95a00e135dd4
mbantwerpes <marcel.blesing@antwerpes.com>
2021-09-15 14:53:05 +0200

    Secure order routes

--- src/lib/routes/order.ts
+++ src/lib/routes/order.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import authAdmin from '../middleware/auth';
 import { getOrders, getOrder, addOrder, updateOrder } from '../models/order';
 import type { TechComponentOrder } from '../types/types';
 
@@ -33,7 +34,7 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', async (req, res) => {
+router.put('/:id', authAdmin, async (req, res) => {
   try {
     const id = req.params.id;
     const orderData = req.body;
