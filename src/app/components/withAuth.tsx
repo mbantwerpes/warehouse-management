4c4469055b9fdadc0086cdbeff43ce30cedab90f
mbantwerpes <marcel.blesing@antwerpes.com>
2021-09-16 16:23:09 +0200

    Change WithAuth to functional component

--- src/app/components/withAuth.tsx
+++ src/app/components/withAuth.tsx
@@ -1,33 +1,32 @@
 // withAuth.jsx
-import React, { useEffect, useState } from 'react';
-import { Redirect } from 'react-router-dom';
+import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
 import { useUserContext } from '../context/UserContext';
 
-const withAuth = (
-  ComponentToProtect: React.FC
-  // checkAdmin = false
-): React.ReactNode => {
-  const [isLoading, setIsLoading] = useState(true);
-
+const WithAuth = ({
+  ComponentToProtect,
+}: {
+  ComponentToProtect: React.ElementType;
+}): JSX.Element => {
   const { role } = useUserContext();
-  console.log(role);
-  useEffect(() => {
-    if (role) {
-      console.log('im effect');
-      setIsLoading(false);
-    }
-  }, [role, useUserContext]);
 
-  if (isLoading) {
-    return <p>Loading</p>;
-  }
-
-  if (role) {
-    console.log('protected');
-    return <ComponentToProtect />;
-  } else {
-    return <Redirect to="/login" />;
-  }
+  return (
+    <Route
+      render={(props) => {
+        if (role) {
+          return <ComponentToProtect {...props} />;
+        } else {
+          return (
+            <Redirect
+              to={{
+                pathname: '/login',
+              }}
+            />
+          );
+        }
+      }}
+    />
+  );
 };
 
-export default withAuth;
+export default WithAuth;
