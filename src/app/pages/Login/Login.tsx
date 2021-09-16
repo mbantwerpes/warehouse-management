ad9122c26e320ffa68d8f966c1f92fc59032b1b5
mbantwerpes <marcel.blesing@antwerpes.com>
2021-09-16 20:13:31 +0200

    Refactor login

--- src/app/pages/Login/Login.tsx
+++ src/app/pages/Login/Login.tsx
@@ -37,7 +37,7 @@ const Login = (): JSX.Element => {
         onSubmit={handleSubmit}
         setEmailValue={setEmailValue}
         setPasswordValue={setPasswordValue}
-      ></LoginForm>
+      />
     </div>
   );
 };
