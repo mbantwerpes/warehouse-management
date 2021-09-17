b58472964220ee63c1a47688be9de2de82db79b1
mbantwerpes <marcel.blesing@antwerpes.com>
2021-09-17 13:17:11 +0200

    Add className to Typography component

--- src/app/components/Typography/Typography.tsx
+++ src/app/components/Typography/Typography.tsx
@@ -5,27 +5,69 @@ export type TypographyProps = {
   size: 'xs' | 's' | 'm' | 'l' | 'xl';
   type: 'header' | 'text';
   children: ReactNode;
+  className?: string;
 };
 
 const Typography = ({
   size = 'm',
   type = 'text',
   children,
+  className,
 }: TypographyProps): JSX.Element => {
   const sizeTextMap = {
-    xs: <p className={`${styles.text} ${styles.textXS}`}>{children}</p>,
-    s: <p className={`${styles.text} ${styles.textS}`}>{children}</p>,
-    m: <p className={`${styles.text} ${styles.textM}`}>{children}</p>,
-    l: <p className={`${styles.text} ${styles.textL}`}>{children}</p>,
-    xl: <p className={`${styles.text} ${styles.textXL}`}>{children}</p>,
+    xs: (
+      <p className={`${styles.text} ${styles.textXS} ${className}`}>
+        {children}
+      </p>
+    ),
+    s: (
+      <p className={`${styles.text} ${styles.textS} ${className}`}>
+        {children}
+      </p>
+    ),
+    m: (
+      <p className={`${styles.text} ${styles.textM} ${className}`}>
+        {children}
+      </p>
+    ),
+    l: (
+      <p className={`${styles.text} ${styles.textL} ${className}`}>
+        {children}
+      </p>
+    ),
+    xl: (
+      <p className={`${styles.text} ${styles.textXL} ${className}`}>
+        {children}
+      </p>
+    ),
   };
 
   const sizeHeaderMap = {
-    xs: <h5 className={`${styles.header} ${styles.headerXS}`}>{children}</h5>,
-    s: <h4 className={`${styles.header} ${styles.headerS}`}>{children}</h4>,
-    m: <h3 className={`${styles.header} ${styles.headerM}`}>{children}</h3>,
-    l: <h2 className={`${styles.header} ${styles.headerL}`}>{children}</h2>,
-    xl: <h1 className={`${styles.header} ${styles.headerXL}`}>{children}</h1>,
+    xs: (
+      <h5 className={`${styles.header} ${styles.headerXS} ${className}`}>
+        {children}
+      </h5>
+    ),
+    s: (
+      <h4 className={`${styles.header} ${styles.headerS} ${className}`}>
+        {children}
+      </h4>
+    ),
+    m: (
+      <h3 className={`${styles.header} ${styles.headerM} ${className}`}>
+        {children}
+      </h3>
+    ),
+    l: (
+      <h2 className={`${styles.header} ${styles.headerL} ${className}`}>
+        {children}
+      </h2>
+    ),
+    xl: (
+      <h1 className={`${styles.header} ${styles.headerXL} ${className}`}>
+        {children}
+      </h1>
+    ),
   };
 
   if (type === 'header') {
