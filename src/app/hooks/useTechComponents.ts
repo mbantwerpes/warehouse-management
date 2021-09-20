27356c2cc094740e06675082925b39e7dc6cdb50
mbantwerpes <marcel.blesing@antwerpes.com>
2021-09-20 07:57:33 +0200

    Refactor useTechComponents hook

--- src/app/hooks/useTechComponents.ts
+++ src/app/hooks/useTechComponents.ts
@@ -3,29 +3,33 @@ import axios from 'axios';
 import { TechComponent } from '../../lib/types/types';
 import { UseQueryResult } from 'react-query';
 
-export default function useTechComponents(
+const getTechComponents = async (searchValue?: string, ids?: string[]) => {
+  let url = '/api/techcomponent?';
+  if (searchValue) {
+    url += `searchValue=${searchValue}`;
+  }
+  if (ids) {
+    ids.forEach((id, index) => {
+      if (index === ids.length - 1) {
+        url += `id=${id}`;
+      } else {
+        url += `id=${id}&`;
+      }
+    });
+  }
+
+  const { data } = await axios.get(url);
+  return data;
+};
+
+const useTechComponents = (
   searchValue?: string,
   ids?: string[]
-): UseQueryResult<TechComponent[], Error> {
+): UseQueryResult<TechComponent[], Error> => {
   return useQuery<TechComponent[], Error>(
     ['techComponents', searchValue, ids],
-    async () => {
-      let url = '/api/techcomponent?';
-      if (searchValue) {
-        url += `searchValue=${searchValue}`;
-      }
-      if (ids) {
-        ids.forEach((id, index) => {
-          if (index === ids.length - 1) {
-            url += `id=${id}`;
-          } else {
-            url += `id=${id}&`;
-          }
-        });
-      }
-
-      const { data } = await axios.get(url);
-      return data;
-    }
+    () => getTechComponents(searchValue, ids)
   );
-}
+};
+
+export default useTechComponents;
