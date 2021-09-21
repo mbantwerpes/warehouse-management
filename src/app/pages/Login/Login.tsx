dc357072b694c099b9d6737c88eee5aa40d9f0da
mbantwerpes <marcel.blesing@antwerpes.com>
2021-09-21 14:14:57 +0200

    Add styling to login page

--- src/app/pages/Login/Login.tsx
+++ src/app/pages/Login/Login.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { useMutation } from 'react-query';
 import { useHistory } from 'react-router-dom';
 import LoginForm from '../../components/LoginForm/LoginForm';
+import styles from './Login.module.css';
 
 const Login = (): JSX.Element => {
   const history = useHistory();
@@ -31,7 +32,7 @@ const Login = (): JSX.Element => {
   };
 
   return (
-    <div>
+    <div className={styles.container}>
       <LoginForm
         emailValue={emailValue}
         passwordValue={passwordValue}
