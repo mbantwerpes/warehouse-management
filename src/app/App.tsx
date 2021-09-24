a9cf9bd7c488d97ed5014dbe1365dcaeb760108f
mbantwerpes <marcel.blesing@antwerpes.com>
2021-09-24 16:05:15 +0200

    Add rudimentary styling for desktop

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -20,68 +20,76 @@ import 'react-toastify/dist/ReactToastify.min.css';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import StudentProfile from './pages/StudentProfile/StudentProfile';
 import Dashboard from './pages/Dashboard/Dashboard';
+import styles from './App.module.css';
 
 const App = (): JSX.Element => {
   const queryClient = new QueryClient();
 
   return (
-    <AppProvider>
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <Switch>
-            <Route path="/login" component={Login} />
-            <ProtectedRoute
-              ComponentToProtect={UserEdit}
-              path="/user/edit/:id"
-              checkAdmin={true}
-            />
-            <ProtectedRoute
-              ComponentToProtect={UserAdd}
-              path="/user/add"
-              checkAdmin={true}
-            />
-            <ProtectedRoute ComponentToProtect={UserDetail} path="/user/:id" />
-            <ProtectedRoute
-              ComponentToProtect={UserList}
-              path="/user"
-              checkAdmin={false}
-            />
-            <ProtectedRoute
-              ComponentToProtect={StudentProfile}
-              path="/userprofile"
-              checkAdmin={false}
-            />
-            <ProtectedRoute
-              ComponentToProtect={OrderDetail}
-              path="/order/:id"
-            />
-            <ProtectedRoute ComponentToProtect={OrderList} path="/order" />
-            <ProtectedRoute ComponentToProtect={Cart} path="/cart" />
-            <ProtectedRoute
-              path="/techcomponent/add"
-              ComponentToProtect={TechComponentAdd}
-              checkAdmin={true}
-            />
-            <ProtectedRoute
-              path="/techcomponent/edit/:id"
-              ComponentToProtect={TechComponentEdit}
-              checkAdmin={true}
-            />
-            <ProtectedRoute
-              path="/techcomponent/:id"
-              ComponentToProtect={TechComponentDetail}
-            />
-            <ProtectedRoute
-              path="/techcomponent"
-              ComponentToProtect={TechComponentList}
-            />
-            <ProtectedRoute path="/" ComponentToProtect={Dashboard} />
-          </Switch>
-          <ReactQueryDevtools initialIsOpen={false} />
-          <ToastContainer />
-        </BrowserRouter>
-      </QueryClientProvider>
-    </AppProvider>
+    <div className={styles.background}>
+      <div className={styles.maxWidth}>
+        <AppProvider>
+          <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+              <Switch>
+                <Route path="/login" component={Login} />
+                <ProtectedRoute
+                  ComponentToProtect={UserEdit}
+                  path="/user/edit/:id"
+                  checkAdmin={true}
+                />
+                <ProtectedRoute
+                  ComponentToProtect={UserAdd}
+                  path="/user/add"
+                  checkAdmin={true}
+                />
+                <ProtectedRoute
+                  ComponentToProtect={UserDetail}
+                  path="/user/:id"
+                />
+                <ProtectedRoute
+                  ComponentToProtect={UserList}
+                  path="/user"
+                  checkAdmin={false}
+                />
+                <ProtectedRoute
+                  ComponentToProtect={StudentProfile}
+                  path="/userprofile"
+                  checkAdmin={false}
+                />
+                <ProtectedRoute
+                  ComponentToProtect={OrderDetail}
+                  path="/order/:id"
+                />
+                <ProtectedRoute ComponentToProtect={OrderList} path="/order" />
+                <ProtectedRoute ComponentToProtect={Cart} path="/cart" />
+                <ProtectedRoute
+                  path="/techcomponent/add"
+                  ComponentToProtect={TechComponentAdd}
+                  checkAdmin={true}
+                />
+                <ProtectedRoute
+                  path="/techcomponent/edit/:id"
+                  ComponentToProtect={TechComponentEdit}
+                  checkAdmin={true}
+                />
+                <ProtectedRoute
+                  path="/techcomponent/:id"
+                  ComponentToProtect={TechComponentDetail}
+                />
+                <ProtectedRoute
+                  path="/techcomponent"
+                  ComponentToProtect={TechComponentList}
+                />
+                <ProtectedRoute path="/" ComponentToProtect={Dashboard} />
+              </Switch>
+              <ReactQueryDevtools initialIsOpen={false} />
+              <ToastContainer />
+            </BrowserRouter>
+          </QueryClientProvider>
+        </AppProvider>
+      </div>
+    </div>
   );
 };
 
