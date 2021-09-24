be4be4772b1f04942557f444f635efafed95fb8c
mbantwerpes <marcel.blesing@antwerpes.com>
2021-09-24 12:04:44 +0200

    Add onclick to logout button

--- src/app/pages/Dashboard/Dashboard.tsx
+++ src/app/pages/Dashboard/Dashboard.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
+import Button from '../../components/Button/Button';
 import Navbar from '../../components/Navbar/Navbar';
 import Typography from '../../components/Typography/Typography';
 import styles from './Dashboard.module.css';
 
 const Dashboard = (): JSX.Element => {
+  const handleLogout = () => {
+    console.log('joho');
+  };
+
   return (
     <div className={styles.layout}>
       <div className={styles.container}>
         <Typography type="header" size="xl">
           Dashboard
         </Typography>
+        <Button type="primary" size="l" onClick={handleLogout}>
+          Logout
+        </Button>
       </div>
       <Navbar active="home" />
     </div>
